531871ad6da856e70c1ec825e062d18656fd9f8b
Zaida Hammel <zzaida.hammel@mths.ca>
2022-09-15 15:06:32 +0000

    added hello world file

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
